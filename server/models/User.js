19f8c8d22f5f9fce94700448de73a94b1b77a848
samityasir2005 <shadowrightyt@gmail.com>
2024-06-01 15:00:32 -0400

    added usercontext, createpost

--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ const UserSchema = new mongoose.Schema({
     minlength: 3,
   },
 });
-
+///Hello
 UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
