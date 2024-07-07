c01f631611a2f92609c18210551c09e254542c41
samityasir2005 <shadowrightyt@gmail.com>
2024-07-06 22:44:31 -0400

    fixed create post preferences

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,3 +1,5 @@
+const Post = require("../models/Post");
+
 const createPost = async (req, res) => {
   try {
     const {
@@ -6,17 +8,14 @@ const createPost = async (req, res) => {
       timeOfRideShare,
       seatsAvailable,
       price,
-      winterTires,
-      pets,
-      music,
-      bikes,
       luggage,
+      backRowSeating,
+      otherOptions,
     } = req.body;
 
     // Check if the user has created a post in the last hour
     const oneHourAgo = new Date();
     oneHourAgo.setHours(oneHourAgo.getHours() - 1);
-
     const recentPost = await Post.findOne({
       user: req.user._id,
       createdAt: { $gte: oneHourAgo },
@@ -33,17 +32,14 @@ const createPost = async (req, res) => {
 
     const post = new Post({
       user: req.user._id,
-      username: req.user.name,
       destinationFrom,
       destinationTo,
       timeOfRideShare,
       seatsAvailable,
       price,
-      winterTires,
-      pets,
-      music,
-      bikes,
       luggage,
+      backRowSeating,
+      otherOptions,
       expiresAt,
     });
 
@@ -53,4 +49,80 @@ const createPost = async (req, res) => {
     console.error(error);
     res.status(500).json({ msg: "Server error" });
   }
+};
+
+const getPostDetails = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId).populate(
+      "user",
+      "name"
+    );
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(200).json({
+      post: {
+        ...post.toObject(),
+        username: post.username || post.user.name,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("user", "name")
+      .sort({ createdAt: -1 });
+    res.status(200).json({ posts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+const getUserPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json({ posts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ msg: "Unauthorized" });
+    }
+
+    await Post.findByIdAndDelete(postId);
+    res.status(200).json({ msg: "Post deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+module.exports = {
+  createPost,
+  getPostDetails,
+  getAllPosts,
+  getUserPosts,
+  deletePost,
 };
